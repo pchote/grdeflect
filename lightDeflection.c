3fb3e9a41182043d8992ac641f842948af1e4d8e
Paul Chote <paul@chote.net>
2012-03-16 11:26:36 +1300

    Add license header

--- lightDeflection.c
+++ lightDeflection.c
@@ -1,4 +1,21 @@
 /*
+ * Copyright 2009 Paul Chote
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To link against pgplot, need to complile in two steps:
  * gcc -c main.c
  * gfortran main.o -o test -lm -L/usr/X11R6/lib -lX11 -L/sw/lib -laquaterm -Wl,-framework -Wl,Foundation -L/sw/lib/pgplot -lcpgplot -lpgplot -lpng -lcurses
