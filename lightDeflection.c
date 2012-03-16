15a3a2cc6fbb168d4d5f60941b1dd79c672378d4
Paul Chote <paul@chote.net>
2012-03-16 18:10:43 +1300

    Replace tabs with spaces

--- lightDeflection.c
+++ lightDeflection.c
@@ -46,39 +46,39 @@ double dphi;
 // Derivative of u in terms of phi, u, du
 double diffu(double phi, double u, double du)
 {
-	return du;
+    return du;
 }
 
 // Derivative of du in terms of phi, u, du
 double diffdu(double phi, double u, double du)
 {
-	// d^2u/dphi^2 + u = 3/2*Rg*u^2
-	// if u is dimensionless (u = u*Rg), the Rg drops out
-	return (3.0/2)*u*u-u;
+    // d^2u/dphi^2 + u = 3/2*Rg*u^2
+    // if u is dimensionless (u = u*Rg), the Rg drops out
+    return (3.0/2)*u*u-u;
 }
 
 
 // Shift position by an amount dphi using 4th-order Runge-Kutta algorithm
 void step()
 {
-	// Evaluate derivatives at the start point
-	double k1u = dphi * diffu(phi, u, du);
-	double k1du = dphi * diffdu(phi, u, du);
+    // Evaluate derivatives at the start point
+    double k1u = dphi * diffu(phi, u, du);
+    double k1du = dphi * diffdu(phi, u, du);
 
-	// Calculate values at intermediate points
-	double k2u = dphi * diffu(phi + dphi/2.0, u + k1u/2.0, du + k1du/2.0);
-	double k2du = dphi * diffdu(phi + dphi/2.0, u + k1u/2.0, du + k1du/2.0);
+    // Calculate values at intermediate points
+    double k2u = dphi * diffu(phi + dphi/2.0, u + k1u/2.0, du + k1du/2.0);
+    double k2du = dphi * diffdu(phi + dphi/2.0, u + k1u/2.0, du + k1du/2.0);
 
-	double k3u = dphi * diffu(phi + dphi/2.0, u + k2u/2.0, du + k2du/2.0);
-	double k3du = dphi * diffdu(phi + dphi/2.0, u + k2u/2.0, du + k2du/2.0);
+    double k3u = dphi * diffu(phi + dphi/2.0, u + k2u/2.0, du + k2du/2.0);
+    double k3du = dphi * diffdu(phi + dphi/2.0, u + k2u/2.0, du + k2du/2.0);
 
-	double k4u = dphi * diffu(phi + dphi/2.0, u + k3u/2.0, du + k3du/2.0);
-	double k4du = dphi * diffdu(phi + dphi/2.0, u + k3u/2.0, du + k3du/2.0);
+    double k4u = dphi * diffu(phi + dphi/2.0, u + k3u/2.0, du + k3du/2.0);
+    double k4du = dphi * diffdu(phi + dphi/2.0, u + k3u/2.0, du + k3du/2.0);
 
-	// guess values at end point
-	phi += dphi;
-	u   += (k1u + 2.0 * k2u + 2.0 * k3u + k4u)/6.0;
-	du  += (k1du + 2.0 * k2du + 2.0 * k3du + k4du)/6.0;
+    // guess values at end point
+    phi += dphi;
+    u   += (k1u + 2.0 * k2u + 2.0 * k3u + k4u)/6.0;
+    du  += (k1du + 2.0 * k2du + 2.0 * k3du + k4du)/6.0;
 }
 
 /*
@@ -86,42 +86,42 @@ void step()
  */
 int raytrace(double yi, float zoom, bool saveToFile)
 {
-	// Create an array to hold the calculated data points
-	// Start with 100,000 entries which should be sufficient for most cases.
-	// Array will expand itself if there is too many entries (will happen if the path comes
-	// close to the event horizon and orbits the mass a few times)
-	int dataArraySize = 100000;
-	double *x = malloc(dataArraySize*sizeof(double));
-	double *y = malloc(dataArraySize*sizeof(double));
-	double *bigX, *bigY;
-	int curEntry = 0;
+    // Create an array to hold the calculated data points
+    // Start with 100,000 entries which should be sufficient for most cases.
+    // Array will expand itself if there is too many entries (will happen if the path comes
+    // close to the event horizon and orbits the mass a few times)
+    int dataArraySize = 100000;
+    double *x = malloc(dataArraySize*sizeof(double));
+    double *y = malloc(dataArraySize*sizeof(double));
+    double *bigX, *bigY;
+    int curEntry = 0;
 
-	// Start the photon at least 1000Rs away;
-	double startX = (zoom*aspect > 1000) ? zoom*aspect : 1000;
+    // Start the photon at least 1000Rs away;
+    double startX = (zoom*aspect > 1000) ? zoom*aspect : 1000;
     phi = PI - atan2(yi, startX);
 
     // Initial conditions
-	u   = sin(phi)/yi;
-	du  = cos(phi)/yi;
-	dphi = -(0.001/180)*PI; // decrease phi by 0.001 degrees each step
+    u   = sin(phi)/yi;
+    du  = cos(phi)/yi;
+    dphi = -(0.001/180)*PI; // decrease phi by 0.001 degrees each step
 
     // Record the closest separation and angle
     double bsq = yi*yi;
     double bphi = PI/2;
 
-	// Calculate path
+    // Calculate path
     bool failed = false;
     bool hires = false;
-	while (true)
-	{
-		// Path collapsing to center - stop calculating before the numbers blow up
-		if (zoom*u > 1000)
+    while (true)
+    {
+        // Path collapsing to center - stop calculating before the numbers blow up
+        if (zoom*u > 1000)
         {
             failed = true;
-			break;
+            break;
         }
-		x[curEntry] = cos(phi)/u;
-		y[curEntry] = sin(phi)/u;
+        x[curEntry] = cos(phi)/u;
+        y[curEntry] = sin(phi)/u;
 
         // Squared distance to the center of mass
         double dsq = x[curEntry]*x[curEntry] + y[curEntry]*y[curEntry];
@@ -143,36 +143,36 @@ int raytrace(double yi, float zoom, bool saveToFile)
             hires = false;
         }
 
-		// Path has (at least) reached viewport position, and has passed outside the viewport
+        // Path has (at least) reached viewport position, and has passed outside the viewport
         // Calculate points to at least 10rs to ensure the path is straight so provide an accurate estimate of deflection angle
-		// Also ensure we have at least one point outside the viewport for the line to finish on
-		if (phi < PI/2 && (dsq > 100) && ((abs(x[curEntry-1]) > zoom*aspect || abs(y[curEntry-1]) > zoom)))
-			break;
+        // Also ensure we have at least one point outside the viewport for the line to finish on
+        if (phi < PI/2 && (dsq > 100) && ((abs(x[curEntry-1]) > zoom*aspect || abs(y[curEntry-1]) > zoom)))
+            break;
 
-		curEntry++;
+        curEntry++;
 
-		// Too many entries, need to expand the data array
-		if (curEntry == dataArraySize)
-		{
-			//printf("Expanding data array %d -> %d entries\n", dataArraySize, 10*dataArraySize);
-			bigX = malloc(10*dataArraySize*sizeof(double));
-			bigY = malloc(10*dataArraySize*sizeof(double));
+        // Too many entries, need to expand the data array
+        if (curEntry == dataArraySize)
+        {
+            //printf("Expanding data array %d -> %d entries\n", dataArraySize, 10*dataArraySize);
+            bigX = malloc(10*dataArraySize*sizeof(double));
+            bigY = malloc(10*dataArraySize*sizeof(double));
 
-			int i;
-			for (i = 0; i < dataArraySize; i++)
-			{
-				bigX[i] = x[i];
-				bigY[i] = y[i];
-			}
-			dataArraySize *= 10;
-			free(x);
-			free(y);
-			x = bigX;
-			y = bigY;
-		}
+            int i;
+            for (i = 0; i < dataArraySize; i++)
+            {
+                bigX[i] = x[i];
+                bigY[i] = y[i];
+            }
+            dataArraySize *= 10;
+            free(x);
+            free(y);
+            x = bigX;
+            y = bigY;
+        }
 
-		step();
-	}
+        step();
+    }
 
     // Closest separation between photon and center of mass
     double b = sqrt(bsq);
@@ -196,44 +196,44 @@ int raytrace(double yi, float zoom, bool saveToFile)
     }
 
     // Draw path to screen or ps file
-	char filename[20];
-	char device[25];
+    char filename[20];
+    char device[25];
 
-	if (saveToFile)
-	{
-		sprintf(filename, "path-%.3f.ps",yi);
-		sprintf(device, "%s/PS",filename);
-	}
-	else
-		strcpy(device, "9/xs");
+    if (saveToFile)
+    {
+        sprintf(filename, "path-%.3f.ps",yi);
+        sprintf(device, "%s/PS",filename);
+    }
+    else
+        strcpy(device, "9/xs");
 
-	if(cpgbeg(0, device, 1, 1) != 1)
-		return EXIT_FAILURE;
+    if(cpgbeg(0, device, 1, 1) != 1)
+        return EXIT_FAILURE;
 
-	// Setup page, draw axes
-	cpgpage();
-	cpgsvp(0.05, 0.95, 0.1, 0.95);
-	cpgwnad(-aspect*zoom, aspect*zoom, -zoom, zoom);
-	cpgbox("bcnts", 0.0, 0, "bcntsv", 0.0, 0);
+    // Setup page, draw axes
+    cpgpage();
+    cpgsvp(0.05, 0.95, 0.1, 0.95);
+    cpgwnad(-aspect*zoom, aspect*zoom, -zoom, zoom);
+    cpgbox("bcnts", 0.0, 0, "bcntsv", 0.0, 0);
 
-	cpgscf(2); // Set font style to roman
-	cpgslw(2); // Set line width 2
+    cpgscf(2); // Set font style to roman
+    cpgslw(2); // Set line width 2
 
-	// Plot title
-	char *titleString;
-	asprintf(&titleString, "Path for photon with impact parameter %.3f", yi);
-	cpgmtxt("t", 1.0, 0.5, 0.5, titleString);
+    // Plot title
+    char *titleString;
+    asprintf(&titleString, "Path for photon with impact parameter %.3f", yi);
+    cpgmtxt("t", 1.0, 0.5, 0.5, titleString);
     free(titleString);
 
-	// Plot event horizon
-	cpgsfs(2); // Set fill style to outline
-	cpgsls(2); // Set line style to dashed
-	cpgsci(2); // Set colour to red
-	cpgcirc(0, 0, 1);
+    // Plot event horizon
+    cpgsfs(2); // Set fill style to outline
+    cpgsls(2); // Set line style to dashed
+    cpgsci(2); // Set colour to red
+    cpgcirc(0, 0, 1);
 
     // Create a float array for pgplot
     float *xf = malloc(curEntry*sizeof(float));
-	float *yf = malloc(curEntry*sizeof(float));
+    float *yf = malloc(curEntry*sizeof(float));
     for (int i = 0; i < curEntry; i++)
     {
         xf[i] = x[i];
@@ -260,19 +260,19 @@ int raytrace(double yi, float zoom, bool saveToFile)
     cpgsls(1); // Set line style to normal
     cpgsci(7); // Set colour to yellow
     cpgline(curEntry,xf,yf);
-	cpgend();
+    cpgend();
 
-	if (saveToFile)
-	{
-		clear();
-		printw("Saved to file '%s'\n",filename);
-		printw("\n(press any key to continue)\n");
-		getch();
-	}
+    if (saveToFile)
+    {
+        clear();
+        printw("Saved to file '%s'\n",filename);
+        printw("\n(press any key to continue)\n");
+        getch();
+    }
 
-	free(x);
-	free(y);
-	return EXIT_SUCCESS;
+    free(x);
+    free(y);
+    return EXIT_SUCCESS;
 }
 
 /*
@@ -280,76 +280,76 @@ int raytrace(double yi, float zoom, bool saveToFile)
  */
 void printHelp()
 {
-	clear();
-	attron(A_BOLD);
-	printw("grdeflect ");
-	attroff(A_BOLD);
-	printw(versionString);
-	printw("\nby Paul Chote for Victoria University of Wellington\n");
-	int row = 3;
-	int cola = 3;
-	int colb = 10;
-	move(row,cola);
-	attron(A_BOLD);
-	printw("COMMANDS");
-	attroff(A_BOLD);
+    clear();
+    attron(A_BOLD);
+    printw("grdeflect ");
+    attroff(A_BOLD);
+    printw(versionString);
+    printw("\nby Paul Chote for Victoria University of Wellington\n");
+    int row = 3;
+    int cola = 3;
+    int colb = 10;
+    move(row,cola);
+    attron(A_BOLD);
+    printw("COMMANDS");
+    attroff(A_BOLD);
 
-	move(++row,cola);
-	attron(A_BOLD);
-	printw("?");
-	attroff(A_BOLD);
-	move(row,colb);
-	printw("Display this list.");
-	++row;
+    move(++row,cola);
+    attron(A_BOLD);
+    printw("?");
+    attroff(A_BOLD);
+    move(row,colb);
+    printw("Display this list.");
+    ++row;
 
-	move(++row,cola);
-	attron(A_BOLD);
-	printw("FIRE <impact parameter>");
-	attroff(A_BOLD);
-	move(++row,colb);
-	printw("Fire a photo with specified impact parameter.");
-	++row;
+    move(++row,cola);
+    attron(A_BOLD);
+    printw("FIRE <impact parameter>");
+    attroff(A_BOLD);
+    move(++row,colb);
+    printw("Fire a photo with specified impact parameter.");
+    ++row;
 
-	move(++row,cola);
-	attron(A_BOLD);
-	printw("VIEWPORT <maximum radius>");
-	attroff(A_BOLD);
-	move(++row,colb);
-	printw("Adjust the zoom of the viewport to this radius.");
-	++row;
+    move(++row,cola);
+    attron(A_BOLD);
+    printw("VIEWPORT <maximum radius>");
+    attroff(A_BOLD);
+    move(++row,colb);
+    printw("Adjust the zoom of the viewport to this radius.");
+    ++row;
 
-	move(++row,cola);
-	attron(A_BOLD);
-	printw("PRINT");
-	attroff(A_BOLD);
-	move(row,colb);
-	printw("Save current graph to file.");
-	++row;
+    move(++row,cola);
+    attron(A_BOLD);
+    printw("PRINT");
+    attroff(A_BOLD);
+    move(row,colb);
+    printw("Save current graph to file.");
+    ++row;
 
-	move(++row,cola);
-	attron(A_BOLD);
-	printw("QUIT");
-	attroff(A_BOLD);
-	move(row,colb);
-	printw("Exit the program.");
+    move(++row,cola);
+    attron(A_BOLD);
+    printw("QUIT");
+    attroff(A_BOLD);
+    move(row,colb);
+    printw("Exit the program.");
 
-	move(row+2, 0);
-	printw("(press any key to continue)");
+    move(row+2, 0);
+    printw("(press any key to continue)");
 
-	refresh();
+    refresh();
 
-	getch();
+    getch();
 
-	clear();
+    clear();
 }
 
 void printError(char *msg)
 {
-	clear();
-	printw(msg);
-	printw("\n\n(press any key to continue)");
+    clear();
+    printw(msg);
+    printw("\n\n(press any key to continue)");
 
-	getch();
+    getch();
 }
 
 /*
@@ -364,82 +364,82 @@ void printError(char *msg)
 
 int main(int argc, char *argv[])
 {
-	initscr();				/* start the curses mode */
-	echo();					/* show user input */
+    initscr();              /* start the curses mode */
+    echo();                 /* show user input */
 
-	double b = 3.0f;
-	float zoom = 5.0f;
+    double b = 3.0f;
+    float zoom = 5.0f;
 
-	double inputb;
-	float inputzoom;
-	bool doRaytrace = false;
-	char inputBuffer[20];
+    double inputb;
+    float inputzoom;
+    bool doRaytrace = false;
+    char inputBuffer[20];
 
-	while (true)
+    while (true)
     {
-		if (doRaytrace)
-		{
-			raytrace(b, zoom, false);
-			doRaytrace = 0;
-		}
-		clear();			/* clear the screen */
-		printw("Enter Command (type \"?\" for help):\n");
+        if (doRaytrace)
+        {
+            raytrace(b, zoom, false);
+            doRaytrace = 0;
+        }
+        clear();            /* clear the screen */
+        printw("Enter Command (type \"?\" for help):\n");
 
-		getstr(inputBuffer); // Get the user's reply
+        getstr(inputBuffer); // Get the user's reply
 
-		if (strcasecmp("QUIT", inputBuffer) == 0)
-		{
-			break;
-		}
+        if (strcasecmp("QUIT", inputBuffer) == 0)
+        {
+            break;
+        }
 
-		if (strcasecmp("?", inputBuffer) == 0)
-		{
-			printHelp();
-			continue;
-		}
+        if (strcasecmp("?", inputBuffer) == 0)
+        {
+            printHelp();
+            continue;
+        }
 
-		if (strcasecmp("PRINT", inputBuffer) == 0)
-		{
-			raytrace(b, zoom, true);
-			continue;
-		}
+        if (strcasecmp("PRINT", inputBuffer) == 0)
+        {
+            raytrace(b, zoom, true);
+            continue;
+        }
 
-		if (strncasecmp("FIRE ", inputBuffer,5) == 0)
-		{
-			inputb = -1;
-			sscanf(inputBuffer,"%*s %lf",&inputb);
-			if (b != inputb)
-			{
-				if (inputb >= 0)
-				{
-					b = inputb;
-					doRaytrace = 1;
-				}
-				else
-					printError("Invalid impact parameter specified.");
-			}
-			continue;
-		}
+        if (strncasecmp("FIRE ", inputBuffer,5) == 0)
+        {
+            inputb = -1;
+            sscanf(inputBuffer,"%*s %lf",&inputb);
+            if (b != inputb)
+            {
+                if (inputb >= 0)
+                {
+                    b = inputb;
+                    doRaytrace = 1;
+                }
+                else
+                    printError("Invalid impact parameter specified.");
+            }
+            continue;
+        }
 
-		if (strncasecmp("VIEWPORT ", inputBuffer,9) == 0)
-		{
-			inputzoom = -1;
-			sscanf(inputBuffer,"%*s %f",&inputzoom);
-			if (zoom != inputzoom)
-			{
-				if (inputzoom > 0)
-				{
-					zoom = inputzoom;
-					doRaytrace = 1;
-				}
-				else
-					printError("Invalid zoom specified.");
-			}
-			continue;
-		}
-		printError("Unknown command");
-	}
+        if (strncasecmp("VIEWPORT ", inputBuffer,9) == 0)
+        {
+            inputzoom = -1;
+            sscanf(inputBuffer,"%*s %f",&inputzoom);
+            if (zoom != inputzoom)
+            {
+                if (inputzoom > 0)
+                {
+                    zoom = inputzoom;
+                    doRaytrace = 1;
+                }
+                else
+                    printError("Invalid zoom specified.");
+            }
+            continue;
+        }
+        printError("Unknown command");
+    }
 
-	endwin();				/* end curses mode */
-	return EXIT_SUCCESS;
+    endwin();               /* end curses mode */
+    return EXIT_SUCCESS;
 }
